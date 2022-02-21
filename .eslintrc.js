1ebbe0180d8814a50700b576db755276e8265ce0
Spenser Black <spenserblack01@gmail.com>
2022-02-20 22:12:19 -0500

    Add airbnb typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
