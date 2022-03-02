8117e9eddb03c870579623cd7340f31dc428ce6b
Spenser Black <spenserblack01@gmail.com>
2022-03-02 13:48:34 -0500

    squash! TEMP Define palette

--- src/palette.ts
+++ src/palette.ts
@@ -1,9 +1,25 @@
 import type Color from './color';
 
-export default class Palette<C extends Color> {
-  readonly colors: C[];
+export type PaletteColors<C extends Color> = Array<C>;
 
-  constructor(colors: C[]) {
+export interface OneBitColors<C extends Color> extends PaletteColors<C> {
+  length: 2;
+}
+
+export interface TwoBitColors<C extends Color> extends PaletteColors<C> {
+  length: 4;
+}
+
+export interface EightBitColors<C extends Color> extends PaletteColors<C> {
+  length: 256;
+}
+
+export type ColorList<C extends Color> = OneBitColors<C> | TwoBitColors<C> | EightBitColors<C>;
+
+export default class Palette<C extends Color> {
+  readonly colors: ColorList<C>;
+
+  constructor(colors: ColorList<C>) {
     this.colors = colors;
   }
 }
