6aee8a5ee5a7fa67b7ba59cf4db29fcbc49b63a0
Spenser Black <spenserblack01@gmail.com>
2022-03-28 16:08:57 -0400

    Test palettes

--- __tests__/palette.test.ts
+++ __tests__/palette.test.ts
@@ -1,19 +1,71 @@
 import OneBitPalette from '../src/palette/one-bit';
-import { Grayscale } from '@imretro/color';
+import TwoBitPalette from '../src/palette/two-bit';
+import EightBitPalette from '../src/palette/eight-bit';
+import * as color from '@imretro/color';
 
 describe('Palette', () => {
   describe('OneBitPalette', () => {
     describe('convert', () => {
-      const palette = new OneBitPalette(new Grayscale(0xFF), new Grayscale(0));
+      const palette = new OneBitPalette(new color.Grayscale(0xFF), new color.Grayscale(0));
 
-      const dark = new Grayscale(0x40);
-      const light = new Grayscale(0xB0);
+      const dark = new color.Grayscale(0x40);
+      const light = new color.Grayscale(0xB0);
 
       test.each([
         [dark, 0xFF, 0xFF, 0xFF, 0xFF],
         [light, 0, 0, 0, 0xFF],
       ])('%p converted is [%i, %i, %i, %i]', (color, er, eg, eb, ea) => {
         const {r, g, b, a } = palette.convert(color);
+
+        expect(r).toBe(er);
+        expect(g).toBe(eg);
+        expect(b).toBe(eb);
+        expect(a).toBe(ea);
+      });
+    });
+  });
+
+  describe('TwoBitPalette', () => {
+    describe('convert', () => {
+      const colors = [
+        new color.Grayscale(0xFF),
+        new color.Grayscale(0xAA),
+        new color.Grayscale(0x55),
+        new color.Grayscale(0),
+      ];
+      const palette = new TwoBitPalette(colors);
+
+      const darkest = new color.Alpha(0x55);
+      const dark = new color.Grayscale(0x56);
+      const bright = new color.Grayscale(0xB0);
+      const brightest = new color.RGBA(0xFF, 0, 0, 0xFF);
+
+      test.each([
+        [darkest, 0xFF, 0xFF, 0xFF, 0xFF],
+        [dark, 0xAA, 0xAA, 0xAA, 0xFF],
+        [bright, 0x55, 0x55, 0x55, 0xFF],
+        [brightest, 0, 0, 0, 0xFF],
+      ])('%p converted is [%i, %i, %i, %i]', (color, er, eg, eb, ea) => {
+        const { r, g, b, a } = palette.convert(color);
+
+        expect(r).toBe(er);
+        expect(g).toBe(eg);
+        expect(b).toBe(eb);
+        expect(a).toBe(ea);
+      });
+    });
+  });
+
+  describe('EightBitPalette', () => {
+    describe('convert', () => {
+      const colors = Array.from({ length: 256 }).map((_, index) => new color.Grayscale(index));
+      const palette = new EightBitPalette(colors);
+
+      test.each([
+        [new color.RGBA(0, 0, 0, 0xFF), 0b11000000, 0b11000000, 0b11000000, 0xFF],
+        [new color.RGBA(0b01 << 6, 0, 0, 0), 1, 1, 1, 0xFF],
+      ])('%p converted is [%i, %i, %i, %i]', (color, er, eg, eb, ea) => {
+        const { r, g, b, a } = palette.convert(color);
 
         expect(r).toBe(er);
         expect(g).toBe(eg);
