47ca01cc059fb0550cf9f238680a3c418eb602fe
Spenser Black <spenserblack01@gmail.com>
2022-05-27 13:32:31 -0400

    Add tests for expected encoded byte count

---
+++ __tests__/encode.test.ts
@@ -0,0 +1,22 @@
+import { Image } from '../src/index';
+import * as flags from '../src/flags';
+import * as palettes from '../src/palette';
+
+describe('Image', () => {
+  describe('encode', () => {
+    test.each([
+      [new Image(flags.PixelMode.OneBit, 1, 1, palettes.default1Bit, [0]), 12],
+      [new Image(flags.PixelMode.OneBit, 2, 2, palettes.default1Bit, Array(4).fill(0)), 12],
+      [new Image(flags.PixelMode.OneBit, 3, 4, palettes.default1Bit, Array(12).fill(0)), 13],
+      [new Image(flags.PixelMode.TwoBit, 1, 1, palettes.default1Bit, [0]), 12],
+      [new Image(flags.PixelMode.TwoBit, 2, 2, palettes.default1Bit, Array(4).fill(0)), 12],
+      [new Image(flags.PixelMode.TwoBit, 3, 4, palettes.default1Bit, Array(12).fill(0)), 14],
+      [new Image(flags.PixelMode.EightBit, 1, 1, palettes.default1Bit, [0]), 12],
+      [new Image(flags.PixelMode.EightBit, 2, 2, palettes.default1Bit, Array(4).fill(0)), 15],
+      [new Image(flags.PixelMode.EightBit, 3, 4, palettes.default1Bit, Array(12).fill(0)), 23],
+    ])('%p encoded should be %d bytes', (image, expected) => {
+      const byteView = new Uint8Array(image.encode());
+      expect(byteView.length).toBe(expected);
+    });
+  });
+});
